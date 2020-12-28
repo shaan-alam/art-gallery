66de1e2133a49e060eb69d4ac78ff703aa2b7a70
shaan <shaanalam369@gmail.com>
2020-12-28 13:08:01 +0530

    responsive navbar

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,4 @@
-import { connect } from "react-redux";
-import { useEffect } from "react";
-import { logout } from "../Redux/Actions/AuthActionCreators";
+
 import Navbar from "./Navbar";
 
 const Home = () => {
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,32 +1,81 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
-import NavbarDropdown from "./NavbarDropdown";
+import { Link, useHistory } from "react-router-dom";
+import { connect } from "react-redux";
+import { logout } from "../Redux/Actions/AuthActionCreators";
 
-const Navbar = () => {
-  const [navmenu, setNavmenu] = useState(false);
+const Navbar = ({ currentUser, logout }) => {
+  const [navMenu, setNavMenu] = useState(false);
+  const [navSubMenu, setNavSubMenu] = useState(false);
+
+  const history = useHistory();
+
+  const handleLogout = () => {
+    logout(() => history.push("/login"));
+  };
 
   return (
     <nav>
-      <a
-        href="#!"
-        className="navbar-toggler"
-        onClick={() => setNavmenu(!navmenu)}
-      >
-        <i className="fa fa-bars"></i>
-      </a>
-      <div className="container">
-        <Link to="/" className="heading">
-          <h2>Art Gallery</h2>
-        </Link>
-        <div className={`nav-menu ${navmenu ? "active" : ""}`}>
-          <a href="#!">
-            <i className="fa fa-plus"></i> Upload
-          </a>
-          <NavbarDropdown />
-        </div>
+      <header className="nav-heading">
+        <a
+          href="#!"
+          className="navbar-toggler"
+          onClick={() => setNavMenu(!navMenu)}
+        >
+          <i className="fa fa-bars"></i>
+        </a>
+        <h3>Art gallery</h3>
+      </header>
+      <div className={`nav-menu ${navMenu ? "active" : ""}`}>
+        <ul className="nav-menu-list">
+          <li>
+            <Link to="/">
+              <i className="fa fa-home"></i> Home
+            </Link>
+          </li>
+          <li>
+            <Link>
+              <i className="fa fa-plus"></i> Upload
+            </Link>
+          </li>
+          <li className="user-dropdown">
+            <div className="user-avatar">
+              <h3>{currentUser.displayName}</h3>
+              <img
+                src={currentUser.photoURL}
+                alt={currentUser.displayName}
+                tilte={currentUser.displayName}
+              />
+              <a
+                href="#!"
+                className="submenu-toggler"
+                onClick={() => setNavSubMenu(!navSubMenu)}
+              >
+                <i className="fa fa-chevron-down"></i>
+              </a>
+            </div>
+            <ul
+              className={`user-info-submenu ${
+                navSubMenu ? "active-submenu" : ""
+              }`}
+            >
+              <Link to="profile">
+                <li>View Profile</li>
+              </Link>
+              <Link onClick={handleLogout}>
+                <li> Logout</li>
+              </Link>
+            </ul>
+          </li>
+        </ul>
       </div>
     </nav>
   );
 };
 
-export default Navbar;
+const mapStateToProps = (state) => {
+  return {
+    currentUser: state.auth.currentUser,
+  };
+};
+
+export default connect(mapStateToProps, { logout })(Navbar);
