4cec6358f5850fbbe359c35a830b80112589710e
shaan <shaanalam369@gmail.com>
2020-12-28 11:17:38 +0530

    made navbar responsive

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,24 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import NavbarDropdown from "./NavbarDropdown";
 
 const Navbar = () => {
+  const [navmenu, setNavmenu] = useState(false);
+
   return (
     <nav>
+      <a
+        href="#!"
+        className="navbar-toggler"
+        onClick={() => setNavmenu(!navmenu)}
+      >
+        <i className="fa fa-bars"></i>
+      </a>
       <div className="container">
         <Link to="/" className="heading">
           <h2>Art Gallery</h2>
         </Link>
-        <div className="nav-menu">
+        <div className={`nav-menu ${navmenu ? "active" : ""}`}>
           <a href="#!">
             <i className="fa fa-plus"></i> Upload
           </a>
