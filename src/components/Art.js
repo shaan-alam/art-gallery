134813751f45991cf6164117672027a97909d271
Shaan Alam <shaanalam369@gmail.com>
2021-01-04 18:52:14 +0530

    image grid

--- src/components/Art.js
+++ src/components/Art.js
@@ -1,9 +1,15 @@
+import { motion } from "framer-motion";
+
 const Art = ({ art }) => {
   return (
     <div className="art">
-      <img src={art.url} alt="An Art" />
+      <div className="art-img">
+        <img src={art.url} alt="An Art" />
+      </div>
       <div className="uploaded-by">
-        <img
+        <motion.img
+          initial={{ opacity: 0.8 }}
+          whileHover={{ opacity: 1 }}
           src={art.uploadedBy.photoURL}
           alt={art.uploadedBy.displayName}
           title={art.uploadedBy.displayName}
--- src/components/ImageGrid.js
+++ src/components/ImageGrid.js
@@ -1,32 +1,14 @@
 import { connect } from "react-redux";
 import Art from "./Art";
+import { motion } from "framer-motion";
 
 const ImageGrid = ({ arts }) => {
-  const col1 = arts.slice(0, Math.floor(0.33 * arts.length));
-  const col2 = arts.slice(
-    Math.floor(0.33 * arts.length) + 1,
-    Math.floor(0.66 * arts.length)
-  );
-  const col3 = arts.slice(Math.floor(0.66 * arts.length) + 1);
-
   return (
-    <div className="art-grid">
-      <div className="art-flex">
-        {col1.map((art) => (
-          <Art art={art} key={art.id} />
-        ))}
-      </div>
-      <div className="art-flex">
-      {col2.map((art) => (
+    <motion.div className="art-grid" layout>
+      {arts.map((art) => (
         <Art art={art} key={art.id} />
       ))}
-      </div>
-      <div className="div-flex">
-      {col3.map((art) => (
-        <Art art={art} key={art.id} />
-      ))}
-      </div>
-    </div>
+    </motion.div>
   );
 };
 
