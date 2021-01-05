6da1e1f5ea06bcc9774792c64fe89ed77ef4cd64
Shaan Alam <shaanalam369@gmail.com>
2021-01-05 12:35:37 +0530

    deployed on firebase & removed modal listener

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,26 +1,6 @@
-import { useEffect } from "react";
 import { motion } from "framer-motion";
 
 const Modal = ({ children, setModal }) => {
-  useEffect(() => {
-    const closeModal = (e) => {
-      if (e.target.classList.contains("modal-backdrop") || e.keyCode === 27) {
-        setModal(false);
-      }
-    };
-
-    // Attach an event listener to window object
-    // If clicked outside the modal, then close the modal
-    window.addEventListener("click", closeModal);
-    window.addEventListener("keydown", closeModal);
-
-    // Unsubscribe from the event listeners
-    return () => {
-      window.removeEventListener("click", closeModal);
-      window.removeEventListener("keydown", closeModal);
-    };
-  }, []);
-
   return (
     <motion.div
       className="modal-backdrop"
--- src/firebase/config.js
+++ src/firebase/config.js
@@ -4,13 +4,13 @@ import "firebase/auth";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBJ2BxY1HGpckC_Y88tUhvwnhmHiWQ1kg0",
-  authDomain: "public-art-gallery.firebaseapp.com",
-  projectId: "public-art-gallery",
-  storageBucket: "public-art-gallery.appspot.com",
-  messagingSenderId: "103453354104",
-  appId: "1:103453354104:web:06dc7b381178937186258d",
-  measurementId: "G-LV61XV0SRP",
+  apiKey: "AIzaSyB3JXsYT_9tTAawyqp9mcdPrFfvfcVfGzY",
+  authDomain: "public-art-gallery-4a6db.firebaseapp.com",
+  projectId: "public-art-gallery-4a6db",
+  storageBucket: "public-art-gallery-4a6db.appspot.com",
+  messagingSenderId: "604039083596",
+  appId: "1:604039083596:web:75bef1f8825625a890e6e9",
+  measurementId: "G-4M2R8B8PT8",
 };
 
 // Initialize firebase app
